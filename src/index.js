edb93f834ac9f952e759833feff90ed2f0ea3031
Stefan Junker <mail@stefanjunker.de>
2022-10-11 13:28:52 +0200

    fixup! feat: use existing upterm if available and refactor

--- src/index.js
+++ src/index.js
@@ -52,13 +52,13 @@ export async function run() {
         {id_file:"id_ed25519", algo:"ed25519"}
         ]) {
         if (!fs.existsSync(path.join(sshPath, t.id_file))) {
-          core.info("Generating ${t.algo} SSH key at ${t.id_file}", t.algo);
+          core.info(`Generating ${t.algo} SSH key at ${t.id_file}`);
           try {
-            await execShellCommand(`ssh-keygen -q -t ${t.algo} -N "" -f ~/.ssh/${t.id_file}`);
-          } catch { }    
-          core.info("Generated SSH key for ${t.algo} successfully")
+            await execShellCommand(uti.format(`ssh-keygen -q -t %s -N "" -f ~/.ssh/${t.id_file}`, t.algo);
+          } catch { }
+          core.info(util.format(`Generated SSH key for %ssuccessfully`, ${t.algo}));
         } else {
-          core.info("SSH key for ${t.algo} already exists")
+          core.info(util.format(`SSH key for %s already exists`, t.algo));
         }
     }
 
