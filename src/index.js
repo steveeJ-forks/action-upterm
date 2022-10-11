2d59ad7161809d66d91893fecbba3aba508f52b7
Stefan Junker <mail@stefanjunker.de>
2022-10-11 13:08:53 +0200

    fixup! feat: use existing upterm if available and refactor

--- src/index.js
+++ src/index.js
@@ -52,13 +52,13 @@ export async function run() {
         {id_file:"id_ed25519", algo:"ed25519"}
         ]) {
         if (!fs.existsSync(path.join(sshPath, t.id_file))) {
-          core.debug("Generating ${t.algo} SSH key at ${t.id_file}", t.algo);
+          core.info("Generating ${t.algo} SSH key at ${t.id_file}", t.algo);
           try {
             await execShellCommand(`ssh-keygen -q -t ${t.algo} -N "" -f ~/.ssh/${t.id_file}`);
           } catch { }    
-          core.debug("Generated SSH key for ${t.algo} successfully")
+          core.info("Generated SSH key for ${t.algo} successfully")
         } else {
-          core.debug("SSH key for ${t.algo} already exists")
+          core.info("SSH key for ${t.algo} already exists")
         }
     }
 
@@ -136,7 +136,7 @@ export async function run() {
     console.debug("Entering main loop")
     while (true) {
       try {
-        core.info(await execShellCommand('bash -c "upterm session current --admin-socket ~/.upterm/*.sock"'));
+        core.info(await execShellCommand(`upterm session current --admin-socket ~/.upterm/*.sock`));
       } catch (error) {
         core.info(error.message);
         break
